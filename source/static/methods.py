6b46b9e1a89e3c07f96fed3e14c4c9171f438148
icYFTL <savap0@yandex.ru>
2020-10-20 04:14:48 +0300

    W2J implemented

--- source/static/methods.py
+++ source/static/methods.py
@@ -1,45 +1,21 @@
 from io import BytesIO
-from os import path, mkdir, system, remove
-from random import randint
+from os import path, mkdir
 import requests
-from shutil import move, rmtree
-import sys
+from core import config
+from base64 import encodebytes, decodebytes
+from uuid import uuid4
 
 
-def webp_to_png(img: BytesIO):
-    # webptools doesn't work
+def webp_to_png(img: BytesIO) -> str:
     if not path.exists('tmp'):
         mkdir('tmp')
+    img = encodebytes(img.read()).decode('UTF-8')
+    r = requests.post(config['w2j_url'], json={
+        'key': config['w2j_key'],
+        'image': img
+    })
 
-    if not path.exists('dwebp'):
-        # OK...
-        r = requests.get(
-            'https://files.pythonhosted.org/packages/05/9d/ac0156babdc9ee803bf54b0d47379d98d5f779f0ecaa7f1e199fe39977cf/webptools-0.0.3.tar.gz')
-        open(path.join('tmp', 'webtools.tar.gz'), 'wb').write(r.content)
-        # I don't give a fuck...
-        system(f'tar -xf {path.join("tmp", "webtools.tar.gz")}')
-        move('webptools-0.0.3', path.join('tmp', 'webptools-0.0.3'))
-        remove(path.join('tmp', 'webtools.tar.gz'))
-        src = 'libwebp_linux'
-        if sys.platform == 'darwin':
-            src = 'libwebp_osx'
-        else:
-            src = 'libwebp_win64'
+    filename = f'temp{uuid4().hex}.jpeg'  # I will optimize it later. Probably.
 
-        # I DONT GIVE A FUCK
-
-        move(path.join('tmp', 'webptools-0.0.3', 'lib', src, 'bin', 'dwebp'), '.')
-        rmtree(path.join('tmp', 'webptools-0.0.3'))
-
-    file = f'tempfile{randint(1000, 10000000)}'  # hehe im lazy
-    open(path.join('tmp', file + '.webp'), 'wb').write(img.read())
-    system('dwebp ' + path.abspath(path.join('tmp', file + '.webp')) + ' -o ' + path.abspath(
-        path.join('tmp', file + '.png')))
-
-    # Piece of shit.
-
-    remove(path.join('tmp', file + '.webp'))
-
-    # I hate python. Where is cool webp libraries?
-
-    return path.join('tmp', file + '.png')
+    open(path.join('tmp', filename), 'wb').write(decodebytes(r.text.encode()))
+    return filename
